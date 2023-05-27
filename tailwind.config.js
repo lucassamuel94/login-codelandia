9c3ac4a0feadbff11bbba24e574d458d12af3722
Lucas Samuel <lucassamuel.dev@gmail.com>
2023-05-26 22:22:55 -0300

    🎉 first commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: "'Lato', sans-serif",
+        title: "'Merriweather', serif;",
+      },
+    },
+  },
+  // eslint-disable-next-line no-undef
+  plugins: [require('@tailwindcss/forms')],
+}
