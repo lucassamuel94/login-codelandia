7a004089e7b5d7d2d7f8e427979557e1d18cc809
Lucas Samuel <lucassamuel.dev@gmail.com>
2023-05-26 22:33:49 -0300

    ✨ create routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
-const App = () => {
-  return <div>App</div>
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom'
+import { Error404, Home, Login, ResetPassword } from './pages'
+
+const App: React.FC = () => {
+  const isAuthenticated = false // Substitua por sua lógica de autenticação
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path='/'
+          element={isAuthenticated ? <Home /> : <Navigate to='/login' />}
+        />
+        <Route path='/login' element={<Login />} />
+        <Route path='/reset-password' element={<ResetPassword />} />
+        <Route path='*' element={<Error404 />} />
+      </Routes>
+    </BrowserRouter>
+  )
 }
 
 export default App
