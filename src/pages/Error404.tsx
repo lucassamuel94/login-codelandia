3362eb88addec0817dbda46057ab92f98dbe8c10
Lucas Samuel <lucassamuel.dev@gmail.com>
2023-05-28 11:42:47 -0300

    🎨 update page

--- src/pages/Error404.tsx
+++ src/pages/Error404.tsx
@@ -1,3 +1,19 @@
+import { Link } from 'react-router-dom'
+import { ArrowBack } from '../assets/icons/ArrowBack'
+
 export const Error404 = () => {
-  return <h1>Error 404 - Página não encontrada</h1>
+  return (
+    <div className='min-h-screen flex flex-col gap-1 items-center justify-center '>
+      <h1 className='text-5xl font-bold'>Oops!</h1>
+      <p className='text-xl text-gray-500 mt-2'>Página não encontrada</p>
+
+      <Link
+        to='/'
+        className='hover:underline hover:underline-offset-2 transition-all text-blue-500 mt-8 inline-flex items-center gap-3'
+      >
+        <ArrowBack className='fill-blue-500' />
+        <span>Voltar a navegar</span>
+      </Link>
+    </div>
+  )
 }
